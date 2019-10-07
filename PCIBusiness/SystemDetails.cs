a5c6d92c5682dff272b221b2782b77fb54e09e8e
Paul Kilfoil <paulkilfoil@outlook.com>
2019-10-07 09:21:59 +0200

    Add files via upload

--- PCIBusiness/SystemDetails.cs
+++ PCIBusiness/SystemDetails.cs
@@ -5,8 +5,8 @@
 	//	Now kept in Web.AppDetails ...
 	//	public static string AppName    = "Prosperian Capital Recurring Payments";
 
-		public static string AppVersion = "1.1.016";
-		public static string AppDate    = "2019/10/04 14:58:22";
+		public static string AppVersion = "1.1.017";
+		public static string AppDate    = "2019/10/07 09:14:22";
 		public static string Developer  = "Paul Kilfoil, Software Development & IT Consulting";
 		public static string Owner      = "Prosperian Capital International";
 		public static string OwnerPhone = "+27 21 950 6060";
