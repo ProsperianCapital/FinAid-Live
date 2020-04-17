1e725fcc02d0b63bf0def7c5b595822903c0364a
Paul Kilfoil <paulkilfoil@outlook.com>
2020-04-17 16:56:38 +0200

    Add files via upload

--- PCIWebFinAid/Register.aspx.cs
+++ PCIWebFinAid/Register.aspx.cs
@@ -461,6 +461,10 @@ namespace PCIWebFinAid
 // V2 ... Analytics.js
 						lblGoogleUA.Text = Environment.NewLine
 						                 + "<script>" + Environment.NewLine
+						                 + "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"
+						                 + "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"
+						                 + "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
+						                 + "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');" + Environment.NewLine
 						                 + "ga('create', '" + gaCode + "', 'auto', {'allowLinker': true});" + Environment.NewLine
 						                 + "ga('require', 'linker');" + Environment.NewLine
 						                 + "ga('linker:autoLink', ['" + url + "'] );" + Environment.NewLine
