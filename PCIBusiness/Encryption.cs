5bae97ead571e44300d3188a9a1fbf50d1160d68
Paul Kilfoil <paulkilfoil@outlook.com>
2024-01-29 10:51:16 +0200

    Add files via upload

---
+++ PCIBusiness/Encryption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+// using PgpCore;
+
+namespace PCIBusiness
+{
+	public static class Encryption // Just a stub ... see Encryption(Dev).cs for the work-in-progress code
+	{
+		public static byte Encrypt(string dataPlain,byte method=0)
+		{
+			return 0;
+		}
+	}
+}
